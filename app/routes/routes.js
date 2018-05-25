3a4aea2f0e6caab5355e6ee83a6e4f4a6da2e6ab
frencoret <frencoret@sentra.cl>
2018-05-25 11:41:58 -0400

    'agregando'

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,6 +3,7 @@ var postController = require('../controller/postController');
 
 module.exports = function(app) {
     app.get('/servicios/microservicio/test/v1.0/nombre', nombreController.test);
+    app.get('/servicios/microservicio/test/v1.0/test', testController.probandoTest);
     // microservicio, objeto, versión, acción(nombre)
     app.post('/servicios/microservicio/test/v1.0/postController', postController.test)
     
